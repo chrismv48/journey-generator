d437ab87a6634aada76162c99021f99007325bb1
Chris Armstrong <carmstrong@appnexus.com>
2016-08-11 14:28:02 -0400

    bday message

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 
 export default ({children}) => {
   return (
-    <div id="container">
-      <h1>Under construction</h1>
+    <div id="container" className="container">
+      <h1>Happy Birthday Darling!</h1>
+      <img src="http://bestanimations.com/Holidays/Birthday/funnybithdaygifs/funny-cat-party-happy-birthday-gif.gif"/>
     </div>
   );
 }
